9a4e848cdaa26bef178ebd1da5747324e2f66e09
MrQvest <104365438+MrQvest@users.noreply.github.com>
2023-04-18 10:48:53 -0300

    Testes

--- Kartgame/Assets/Karting/Scripts/KartSystems/ScriptsTutoria/TeleporterScript.cs
+++ Kartgame/Assets/Karting/Scripts/KartSystems/ScriptsTutoria/TeleporterScript.cs
@@ -8,48 +8,77 @@ namespace ModScripts
     {
         public float maxRange = 5f;
         public float teleportTime = 2f;
+
         [SerializeField]
         private LayerMask playerALayer;
         [SerializeField]
         private LayerMask playerBLayer;
-
-        private bool isTeleporting = false;
         private GameObject playerObject;
-        //private Transform position;
+        private int numValidColliders = 0;
+        private Collider[] colliders;
 
-        // Lista de Rigidbody chamada rdbdList
-        List<Rigidbody> rdbdList = new List<Rigidbody>();
-
-        private void Start()
+        public void UseItem(Transform position, Quaternion rotation)
         {
+            playerObject = position.gameObject;
+            Instantiate(gameObject, position.position, rotation);
+            print(playerObject.name);
+            CheckTP();
+        }
 
-            // Procura por todos os objetos contendo Rigidbody
-            foreach (Rigidbody rdbds in FindObjectsOfType<Rigidbody>())
+        void CheckTP()
+        {
+            CheckCollider();
+
+            if (numValidColliders >= 1)
             {
-                // Verifica se eles têm a Tag Player e os adiciona à lista rdbdList
-                if (rdbds.CompareTag("Player"))
+                Teleport();
+            }
+        }
+
+
+
+        void CheckCollider()
+        {
+            // Verifica se há um jogador próximo o suficiente para teleporte
+            colliders = Physics.OverlapSphere(transform.position, maxRange);
+            numValidColliders = 0;
+            foreach (Collider col in colliders)
+            {
+                print(playerObject);
+                print(col.gameObject);
+                // Se os colliders estiverem nas layers PlayerA ou PlayerB e não forem o jogador atual
+                if (col.gameObject.layer != playerObject.layer)
                 {
-                    rdbdList.Add(rdbds);
+                    if (col.gameObject.layer == 15 || col.gameObject.layer == 16)
+                    {
+                        print(col.gameObject.layer);
+                        // Os adiciona à numValidColliders
+                        numValidColliders++;
+                    }
+
                 }
             }
-            //print(rdbdList.Count);
         }
 
-        private void Update()
+        void Teleport()
         {
-            /*if (isTeleporting)
+            // Teleporta para o próximo jogador
+            List<Collider> validColliders = new List<Collider>();
+            foreach (Collider col in colliders)
             {
-                return;
-            }*/
-
-            
+                if ((col.gameObject.layer == LayerMask.NameToLayer("PlayerA") || col.gameObject.layer == LayerMask.NameToLayer("PlayerB")) && col.gameObject != playerObject)
+                {
+                    validColliders.Add(col);
+                }
+            }
+            Collider target = validColliders[0];
+            Debug.Break();
+            print("Active? " + gameObject.activeInHierarchy);
+            StartCoroutine(Teleport(target.transform));
         }
-
 
         private IEnumerator Teleport(Transform target)
         {
-            isTeleporting = true;
-
             // Aguarda o tempo de carregamento
             yield return new WaitForSeconds(teleportTime);
 
@@ -63,17 +92,6 @@ namespace ModScripts
 
             // Mantém a velocidade do kart atual
             playerObject.GetComponent<Rigidbody>().velocity = currentVelocity;
-
-            isTeleporting = false;
-        }
-
-        public void UseItem(Transform position, Quaternion rotation)
-        {
-            playerObject = position.gameObject;
-            Instantiate(gameObject, position.position - (new Vector3(0f, 2f, 0f)), rotation);
-            //print(position.gameObject.name); //Essa linha da print no nome do objeto que usou o item
-            print(playerObject.name);
-            CheckTP();
         }
 
         private void OnDrawGizmosSelected()
@@ -81,44 +99,6 @@ namespace ModScripts
             Gizmos.color = Color.red;
 
             Gizmos.DrawWireSphere(transform.position, maxRange);
-        }
-
-        void CheckTP()
-        {
-            // Verifica se há um jogador próximo o suficiente para teleporte
-            Collider[] colliders = Physics.OverlapSphere(transform.position, maxRange);
-            int numValidColliders = 0;
-            foreach (Collider col in colliders)
-            {
-                print(playerObject);
-                // Se os colliders estiverem nas layers PlayerA ou PlayerB e não forem o jogador atual
-                if (col.gameObject.layer != playerObject.layer)
-                {
-                    if (col.gameObject.layer == 15 || col.gameObject.layer == 16)
-                    {
-                        print(col.gameObject.layer);
-                        // Os adiciona à numValidColliders
-                        numValidColliders++;
-                    }
-
-                }
-            }
-            if (numValidColliders >= 1)
-            {
-                // Teleporta para o próximo jogador
-                List<Collider> validColliders = new List<Collider>();
-                foreach (Collider col in colliders)
-                {
-                    if ((col.gameObject.layer == LayerMask.NameToLayer("PlayerA") || col.gameObject.layer == LayerMask.NameToLayer("PlayerB")) && col.gameObject != playerObject)
-                    {
-                        validColliders.Add(col);
-                    }
-                }
-
-
-                Collider target = validColliders[0];
-                StartCoroutine(Teleport(target.transform));
-            }
         }
     }
 
